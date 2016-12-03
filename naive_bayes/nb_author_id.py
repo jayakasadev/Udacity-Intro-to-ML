33a34a0d2073953e34889fdd4bdb9d33827c16b3
Jaya Kasa <kasa288@gmail.com>
2016-12-03 02:44:48 -0500

    could not get the code to work with python 3.5
    reverted everything back to 2.7
    
    completed code for naive_bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -21,13 +21,28 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
 
+# this line is provided in quiz 2 from udacity
+t0 = time()
 
+classifier.fit(features_train,labels_train)
 
-#########################################################
-### your code goes here ###
+# this line is also provided by udacity in quiz 2
+print "training time:", round(time()-t0, 3), "s"
 
+# this line is provided in quiz 2 from udacity
+t0 = time()
 
-#########################################################
+prediction = classifier.predict(features_test)
 
+# this line is also provided by udacity in quiz 2
+print "prediction time:", round(time()-t0, 3), "s"
+# predictions take 30x less time according to udacity
 
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, prediction)
+
+print "The accuracy of the predictions is %.2f%%" % (accuracy * 100)
+print "The full accuracy value is %f" % accuracy
