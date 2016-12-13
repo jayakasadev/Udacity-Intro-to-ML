56ec861b2344db1510bf8908e05a4d964f129536
Jaya Kasa <kasa288@gmail.com>
2016-12-12 21:25:45 -0500

    completed SVM code
    the reason this project only works in python 2.7 is because some of the data was pickled in python 2.7
    it cannot be recovered in 3.5
    if you really want to
    you can unpickle in 2.7 and repickle in 3.5
    everything should work after that point in 3.5

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,4 +27,66 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+## this code is added to speed up the classification process
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
+# from sklearn.svm import SVC
+# classifier = SVC(kernel="rbf", C=10.0)
+# classifier.fit(features_train, labels_train)
+
+# prediction = classifier.predict(features_test)
+
+# from sklearn.metrics import accuracy_score
+# accuracy = accuracy_score(labels_test,prediction)
+
+# print "C = 10.0"
+# print "The accuracy of the predictions is %.2f%%" % (accuracy * 100)
+# print "The full accuracy value is %f" % accuracy
+
+# classifier = SVC(kernel="rbf", C=100)
+# classifier.fit(features_train, labels_train)
+
+# prediction = classifier.predict(features_test)
+
+# from sklearn.metrics import accuracy_score
+# accuracy = accuracy_score(labels_test,prediction)
+
+# print "C = 100"
+# print "The accuracy of the predictions is %.2f%%" % (accuracy * 100)
+# print "The full accuracy value is %f" % accuracy
+
+# classifier = SVC(kernel="rbf", C=1000)
+# classifier.fit(features_train, labels_train)
+
+# prediction = classifier.predict(features_test)
+
+# from sklearn.metrics import accuracy_score
+# accuracy = accuracy_score(labels_test,prediction)
+
+# print "C = 1000"
+# print "The accuracy of the predictions is %.2f%%" % (accuracy * 100)
+# print "The full accuracy value is %f" % accuracy
+
+from sklearn.svm import SVC
+classifier = SVC(kernel="rbf", C=10000)
+classifier.fit(features_train, labels_train)
+
+prediction = classifier.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test,prediction)
+
+print "C = 10000"
+print "The accuracy of the predictions is %.2f%%" % (accuracy * 100)
+print "The full accuracy value is %f" % accuracy
+
+# print prediction[9]
+print prediction[10]
+# print prediction[25]
+print prediction[26]
+# print prediction[49]
+print prediction[50]
+
+from collections import Counter
+print Counter(prediction)
